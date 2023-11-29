0378f78be477b654990b98b93235bca1bba69b0c
markup54 <marzenakupka@tlen.pl>
2023-11-29 16:56:03 +0100

    wpis do pola edycyjnego

--- Wpf_Formularze_3P_sroda/WindowKwadrat.xaml.cs
+++ Wpf_Formularze_3P_sroda/WindowKwadrat.xaml.cs
@@ -27,8 +27,33 @@ namespace Wpf_Formularze_3P_sroda
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             String wpisanyBok = bok_txt.Text;
+            double bok;
             MessageBox.Show("Wpisano: " + wpisanyBok, "Wartość wpisana",
                 MessageBoxButton.OK, MessageBoxImage.Information);
+            if(double.TryParse(wpisanyBok, out  bok) ) 
+            {
+                double pole = bok * bok;
+                wynik_pole_txt.Text = pole.ToString();
+                double obwod = 4 * bok;
+                wynik_obwod_txt.Text = obwod.ToString();
+
+                kwadrat.Width = bok;
+                kwadrat.Height = bok;
+
+
+            }
+            else
+            {
+                MessageBox.Show("Musisz wpisać liczbę rzeczywistą");
+                wynik_obwod_txt.Text = "";
+                wynik_pole_txt.Text = string.Empty;
+            }
+        }
+
+        private void bok_txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            kwadrat.Width += 20;
+            kwadrat.Height += 20;   
         }
     }
 }
